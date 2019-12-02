d766768a8140465e41bae932a7f99762ad03cde3
Yannis <admin@coding-hard.com>
2019-12-02 23:42:27 +0100

    fixed editfrage

--- editfrage.php
+++ editfrage.php
@@ -22,19 +22,37 @@
 include("config.php");
 $pdo = new PDO("mysql:host=".$db_host.";dbname=".$db_name,$db_user,$db_pass);
 
-$sql = "SELECT id, frage, antwort1, antwort2, antwort3, antwort4, schwer, thema FROM fragen WHERE frage = ?";
-
-$statement = $pdo->prepare($sql);
-
-$editquestion = "";
-
 if (isset($_POST["edit"])) {
     $editquestion = $_POST["edit"];
+    header("Location: editfrage.php?edit=" . $editquestion);
 }
 
+$editquestion = $_GET["edit"] ?? 0; // 0 als Standardwert zum bearbeiten. Evtl. header() mit Location oder Programm exit;
+try {
+    $editId = intval($editquestion);
+} catch (Exception $e) {
+    $editId = 0;
+}
 
-$statement->execute(array($editquestion));
+if (isset($_POST["frage"]) && isset($_POST["editform"])) { // Hier evtl noch mehr abprüfen! Pflichtfelder !!! Dies kann auch mit required im Formularfeld passieren
 
+    $frage = $_POST["frage"];
+    $antwort1 = $_POST["antwort1"];
+    $antwort2 = $_POST["antwort2"];
+    $antwort3 = $_POST["antwort3"];
+    $antwort4 = $_POST["antwort4"];
+    $schwer = $_POST["schwer"];
+    $thema = $_POST["thema"];
+
+    $statement = $pdo->prepare("UPDATE fragen SET frage = ?, antwort1 = ?, antwort2 = ?, antwort3 = ?, antwort4 = ?, schwer = ?, thema = ? WHERE id = ?");
+    $statement->execute(array($frage, $antwort1, $antwort2, $antwort3, $antwort4, $schwer, $thema, $editId)); 
+
+    echo "<p>Die Frage wurde aktualisiert!</p>" . $editId;
+}
+
+$statement = $pdo->prepare("SELECT id, frage, antwort1, antwort2, antwort3, antwort4, schwer, thema FROM fragen WHERE id = ?");
+$editquestion = "";
+$statement->execute(array($editId));
 
 ?>
 
@@ -42,9 +60,10 @@ $statement->execute(array($editquestion));
 <?php while ($row = $statement->fetch()) { ?>
 
     <h2>Frage editieren</h2>
-    <form action='editfrage.php' method='post'>
+    <form action='editfrage.php<?php echo "?edit=".$row['id']; ?>' method='post'>
         ID:<br>
-        <input name='id' type="text" value="<?php echo $row['id'] ?>" disabled>
+        <input name='id' type="number" value="<?php echo $row['id'] ?>" disabled=true>
+        <br>
         Frage:<br>
         <textarea name='frage'><?php echo $row['frage'] ?></textarea>
         <br>
@@ -64,22 +83,19 @@ $statement->execute(array($editquestion));
         <input name='schwer' type='number' value="<?php echo $row['schwer'] ?>">
         <br>
         Thema:<br>
-        <!-- WICHTIG! ALLE ANZEIGEN UND AKTUELLES ALS AUSWAHL EINSTELLEN!!! -->
-        <!-- WICHTIG! ALLE ANZEIGEN UND AKTUELLES ALS AUSWAHL EINSTELLEN!!! -->
-        <!-- WICHTIG! ALLE ANZEIGEN UND AKTUELLES ALS AUSWAHL EINSTELLEN!!! -->
         <select name='thema' value="<?php  ?>">
 
 <?php
 
 foreach($themen as $thema) {
-    echo "<option>" . $thema . "</option>";
+    echo "<option" . ($thema == $row['thema'] ? " selected=true>" : ">") . $thema . "</option>";
 }
 
 ?>
 
         </select>
         <!-- Für Sicherstellung, das die Daten vom Editierungsformular kommen -->
-        <input id="editform" value="1" type="hidden">
+        <input name="editform" value="1" type="hidden">
 
         <br><br>
         <button>Ändern</button>
@@ -89,17 +105,26 @@ foreach($themen as $thema) {
 
 <?php
 
-if (isset($_POST["frage"]) && isset($_POST["antwort1"])
- && isset($_POST["antwort2"]) && isset($_POST["antwort3"]) 
- && isset($_POST["antwort4"]) && isset($_POST["schwer"]) 
- && isset($_POST["thema"]) && isset($_POST["editform"])
- && isset($_POST["id"])) {
+/*
+var_dump(isset($_POST["frage"]));
+var_dump(isset($_POST["antwort1"]));
+var_dump(isset($_POST["antwort2"]));
+var_dump(isset($_POST["antwort3"]));
+var_dump(isset($_POST["antwort4"]));
+var_dump(isset($_POST["thema"]));
+var_dump(isset($_POST["editform"]));
+var_dump(isset($_POST["id"]));
 
-    $statement = $pdo->prepare("UPDATE fragen SET frage = ?, antwort1 = ?, antwort2 = ?, antwort3 = ?, antwort4 = ?, schwer = ?, thema = ? WHERE frage = ?");
-    $statement->execute(array($_POST["frage"], $_POST["antwort1"], $_POST["antwort2"], $_POST["antwort3"], $_POST["antwort4"], $_POST["schwer"], $_POST["id"])); 
+$frage = $_POST["frage"];
+$antwort1 = $_POST["antwort1"];
+$antwort2 = $_POST["antwort2"];
+$antwort3 = $_POST["antwort3"];
+$antwort4 = $_POST["antwort4"];
+$thema = $_POST["thema"];
+$editform = $_POST["editform"];
+$id = $_POST["id"];
 
-    echo "<p>Die Frage wurde aktualisiert</p>";
-}
+*/
 
 ?>
 
