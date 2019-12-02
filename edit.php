989b59e9f7de3edf92e97e789dec4c5704eaa37b
Yannis <admin@coding-hard.com>
2019-12-02 23:41:07 +0100

    added placeholder and fixed output

--- edit.php
+++ edit.php
@@ -27,22 +27,22 @@ $pdo = new PDO("mysql:host=".$db_host.";dbname=".$db_name,$db_user,$db_pass);
     <h2>Hinzufügen</h2>
     <form action='edit.php' method='post'>
         Frage:<br>
-        <textarea name='frage'></textarea>
+        <textarea name='frage' placeholder='Was ist der erste Buchstabe im deutschen Alphabet?'></textarea>
         <br>
         Richtige Antwort:<br>
-        <input name='antwort1' type='text'>
+        <input name='antwort1' type='text' placeholder='A'>
         <br>
         Falsche Antwort:<br>
-        <input name='antwort2' type='text'>
+        <input name='antwort2' type='text' placeholder='N'>
         <br>
         Falsche Antwort:<br>
-        <input name='antwort3' type='text'>
+        <input name='antwort3' type='text' placeholder='R'>
         <br>
         Falsche Antwort:<br>
-        <input name='antwort4' type='text'>
+        <input name='antwort4' type='text' placeholder='X'>
         <br>
         Schwierigkeit:<br>
-        <input name='schwer' type='number'>
+        <input name='schwer' type='number' placeholder=2>
         <br>
         Thema:<br>
         <select name='thema'>
@@ -83,9 +83,9 @@ if (isset($_POST["frage"]) && isset($_POST["antwort1"])
 
 <?php
 
-$sql = "SELECT frage FROM fragen";
+$sql = "SELECT id, frage FROM fragen";
 foreach ($pdo->query($sql) as $row) {
-    echo "<option>" . $row['frage'] . "</option>";
+    echo "<option value='" .  $row['id'] . "'>" . $row['frage'] . "</option>";
 }
 
 ?>
@@ -104,9 +104,9 @@ foreach ($pdo->query($sql) as $row) {
 
 <?php
 
-$sql = "SELECT frage FROM fragen";
+$sql = "SELECT id, frage FROM fragen";
 foreach ($pdo->query($sql) as $row) {
-    echo "<option>" . $row['frage'] . "</option>";
+    echo "<option value='" .  $row['id'] . "'>" . $row['frage'] . "</option>";
 }
 
 ?>
@@ -121,7 +121,7 @@ foreach ($pdo->query($sql) as $row) {
 
 if (isset($_POST["delete"])) {
 
-    $statement = $pdo->prepare("DELETE FROM fragen WHERE frage = ?");
+    $statement = $pdo->prepare("DELETE FROM fragen WHERE id = ?");
     $statement->execute(array($_POST["delete"]));
 
     echo "<p>Die Frage wurde entfernt</p>";
@@ -138,13 +138,13 @@ echo "<h2>Alle Fragen</h2>";
 $sql = "SELECT frage, antwort1, antwort2, antwort3, antwort4, schwer, thema FROM fragen ORDER BY schwer ASC";
 
 foreach ($pdo->query($sql) as $row) {
-    echo "<p>Frage:" . $row['frage'] . "<br>
-         Richtige Antwort:" . $row['antwort1'] . "<br>
-         Falsche Antwort 1:" . $row['antwort2'] . "<br>
-         Falsche Antwort 2:" . $row['antwort3'] . "<br>
-         Falsche Antwort 3:" . $row['antwort4'] . "<br>
-         Frage:" . $row['schwer'] . "<br>
-         Frage:" . $row['thema'] . "<br>
+    echo "<p>Frage: " . $row['frage'] . "<br>
+         Richtige Antwort: " . $row['antwort1'] . "<br>
+         Falsche Antwort 1: " . $row['antwort2'] . "<br>
+         Falsche Antwort 2: " . $row['antwort3'] . "<br>
+         Falsche Antwort 3: " . $row['antwort4'] . "<br>
+         Schwierigkeit: " . $row['schwer'] . "<br>
+         Thema: " . $row['thema'] . "<br>
     ";
 }
 
